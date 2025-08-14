03a0c39781c596522025ee40576e887312c89584
Tiago Silva <programtiago@gmail.com>
2025-08-14 22:57:35 +0100

    Spring Boot backend project

---
+++ backend/src/main/java/com/dev/tiago/infra_tracker_manager_app/backend/InfraTrackerManagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.dev.tiago.infra_tracker_manager_app.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraTrackerManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraTrackerManagerAppApplication.class, args);
+	}
+
+}
